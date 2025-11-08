ceaec1210f7abaf36fc4acea5c2c0d3585a0e14f
Marguerita <winter.madison82@gmail.com>
2025-11-08 14:17:01 -0500

    completed test steps for js file

--- week-3/distance-calculator.js
+++ week-3/distance-calculator.js
@@ -2,9 +2,16 @@
 
 function calculateDistance(planet1, planet2) {
   // Calculates the distance between two planets in AU
-  // given their distances from the Sun in AU.
-  return Math.abs(planet1 - planet2);
-}
-
-module.exports = calculateDistance;
+    // given their distances from the Sun in AU.
+      return Math.abs(planet1 - planet2);
+      }
+      
+      module.exports = calculateDistance;"""use strict";
+      
+      function calculateDistance(planet1, planet2) {
+        // Simple absolute difference between distances from the Sun
+          return Math.abs(planet1 - planet2);
+          }
+          
+          module.exports = calculateDistance;"
 
