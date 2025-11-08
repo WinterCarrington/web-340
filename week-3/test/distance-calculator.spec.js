be300bef01d6ffbc1fb10eb1432b95456dd42e8f
Marguerita <winter.madison82@gmail.com>
2025-11-08 13:49:13 -0500

    added contents of file

--- week-3/test/distance-calculator.spec.js
+++ week-3/test/distance-calculator.spec.js
@@ -1,1 +1,8 @@
+const assert = require('assert');
+const calculateDistance = require('../src/distance-calculator');
 
+function testFunctionDescription() {
+  // TODO: Implement this function
+  }
+
+  // Call your test functions here
