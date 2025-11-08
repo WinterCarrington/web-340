c0a06dcfdc884b8f3a1e7cb3a98fd6389fefc6d7
Marguerita <winter.madison82@gmail.com>
2025-11-08 13:56:27 -0500

    added step é

--- week-3/distance-calculator.js
+++ week-3/distance-calculator.js
@@ -1,6 +1,10 @@
-function calculateDistance(planet1, planet2) {
-      // TODO: Implement this function
-      }
+"use strict";
 
-      module.exports = calculateDistance;
+function calculateDistance(planet1, planet2) {
+  // Calculates the distance between two planets in AU
+  // given their distances from the Sun in AU.
+  return Math.abs(planet1 - planet2);
+}
+
+module.exports = calculateDistance;
 
