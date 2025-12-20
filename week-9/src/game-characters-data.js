7da8135b188577d60887ae2aac2f7343be1ad953
Marguerita <winter.madison82@gmail.com>
2025-12-20 13:11:53 -0500

    Update game-characters-data.js

--- week-9/src/game-characters-data.js
+++ week-9/src/game-characters-data.js
@@ -1,1 +1,21 @@
 // TODO: Implement this script
+
+const characters = [
+  {
+    class: "Warrior",
+    gender: "Female",
+    quirk: "Collects enchanted spoons"
+  },
+  {
+    class: "Mage",
+    gender: "Other",
+    quirk: "Talks to their spellbook"
+  },
+  {
+    class: "Rogue",
+    gender: "Male",
+    quirk: "Steals only left shoes"
+  }
+];
+
+console.log(JSON.stringify(characters));
